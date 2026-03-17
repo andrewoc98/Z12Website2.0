4be3a8c3e3791560afb2931976d3e8abfd3c91fd
Andrew O Connor <173258169+andrewoconnorBetFanatics@users.noreply.github.com>
2026-03-17 20:43:14 +0000

    Update

--- src/app/features/events/lib/mapper.tsx
+++ src/app/features/events/lib/mapper.tsx
@@ -5,9 +5,19 @@ export function mapEvent(id: string, data: FirestoreEventDoc): EventDoc {
     return {
         id,
         name: data.name,
-        startDate: (data.startAt as Timestamp).toDate().toISOString(),
-        endDate: (data.endAt as Timestamp).toDate().toISOString(),
-        closingDate: data.closeAt ? (data.closeAt as Timestamp).toDate().toISOString() : undefined,
+
+        startDate: data.startAt
+            ? data.startAt
+            : null,
+
+        endDate: data.endAt
+            ? data.endAt
+            : null,
+
+        closingDate: data.closeAt instanceof Timestamp
+            ? data.closeAt.toDate().toISOString()
+            : undefined,
+
         status: data.status,
         location: data.location,
         description: data.description,
